18dccff77bad2b1948325a6d19d098631ece6135
kuzRK <sanalsogdanov11188@gmail.com>
2026-09-11 15:46:10 +0300

    [TASK-1] calculator implementation

--- sanal.sogdanov/task-1/cmd/service/main.go
+++ sanal.sogdanov/task-1/cmd/service/main.go
@@ -1,3 +1,43 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	var a, b int
+	var operation string
+
+	_, err := fmt.Scan(&a)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scan(&b)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err = fmt.Scan(&operation)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+	switch operation {
+	case "+":
+		fmt.Println(a + b)
+	case "-":
+		fmt.Println(a - b)
+	case "*":
+		fmt.Println(a * b)
+	case "/":
+		if b == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(a / b)
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
+}
