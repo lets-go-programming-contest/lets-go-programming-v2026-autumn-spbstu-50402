fe1b63c913b3c158246cee45f2fd8c49e4bae133
s1nhronn <silekatik@yandex.ru>
2026-09-16 20:26:05 +0300

    [TASK-1] Entering operands and performing operations, handling invalid input

--- arsenii.shirokov/task-1/cmd/service/main.go
+++ arsenii.shirokov/task-1/cmd/service/main.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	var (
+		a, b int
+		op   string
+	)
+	_, err := fmt.Scanln(&a)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scanln(&b)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err = fmt.Scanln(&op)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
 }
