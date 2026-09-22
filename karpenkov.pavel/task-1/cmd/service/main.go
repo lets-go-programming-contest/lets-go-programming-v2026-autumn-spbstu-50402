719587dd12fba586d0fa257e25d3d452f191cffc
xRonnyn <pashuha.k@gmail.com>
2026-09-22 12:49:26 +0300

    [TASK-1] Moved calculation logic to calculation function

--- karpenkov.pavel/task-1/cmd/service/main.go
+++ karpenkov.pavel/task-1/cmd/service/main.go
@@ -1,39 +1,49 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "errors"
+)
+
+func calculate(a int, b int, operator string) (int,error) {
+  if operator == "+" {
+  		return a + b, nil
+  	} else if operator == "-" {
+  		return a - b, nil
+  	} else if operator == "*" {
+  		return a * b, nil
+  	} else if operator == "/" {
+  		if b == 0 {
+  			return 0,errors.New("Division by zero")
+  		}
+  		return a / b, nil
+  	} else {
+  		return 0,errors.New("Invalid operation")
+  	}
+}
 
 func main() {
 	var a, b int
 	var operator string
 	_, err := fmt.Scanln(&a)
 	if err != nil {
-		fmt.Println("input error -", err)
+		fmt.Println("Invalid first operand")
 		return
 	}
 	_, err = fmt.Scanln(&b)
 	if err != nil {
-		fmt.Println("input error -", err)
+		fmt.Println("Invalid second operand")
 		return
 	}
 	_, err = fmt.Scanln(&operator)
 	if err != nil {
-		fmt.Println("input error -", err)
+		fmt.Println("Invalid operation")
 		return
 	}
-	if operator == "+" {
-		fmt.Println(a + b)
-	} else if operator == "-" {
-		fmt.Println(a - b)
-	} else if operator == "*" {
-		fmt.Println(a * b)
-	} else if operator == "/" {
-		if b == 0 {
-			fmt.Println("Error: division by zero")
-			return
-		}
-		fmt.Println(a / b)
-	} else {
-		fmt.Println("Error: invalid operator")
-		return
-	}
+    res, err1 := calculate(a,b,operator)
+    if err1 == nil{
+        fmt.Println(res)
+    } else{
+        fmt.Println(err1)
+    }
 }
