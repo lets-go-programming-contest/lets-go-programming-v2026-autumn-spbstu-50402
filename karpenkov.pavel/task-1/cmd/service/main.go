2dad18c84c858608a69af0d1cc7c6d0edd3dc899
xRonnyn <pashuha.k@gmail.com>
2026-09-22 11:29:12 +0300

    [TASK-1] Scanning operands and operator; + and - implementation

--- karpenkov.pavel/task-1/cmd/service/main.go
+++ karpenkov.pavel/task-1/cmd/service/main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("init-commit")
+	var a, b int
+	var operand string
+	_, err := fmt.Scanln(&a)
+	if err != nil {
+		fmt.Println("input error -", err)
+		return
+	}
+	_, err = fmt.Scanln(&b)
+	if err != nil {
+		fmt.Println("input error -", err)
+		return
+	}
+	_, err = fmt.Scanln(&operand)
+	if err != nil {
+		fmt.Println("input error -", err)
+		return
+	}
+	if operand == "+" {
+		fmt.Println(a + b)
+	}
+	if operand == "-" {
+		fmt.Println(a - b)
+	}
+
 }
