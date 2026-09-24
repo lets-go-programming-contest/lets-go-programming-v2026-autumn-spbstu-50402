65ea84041016b15361793bb187a0487895c7f4f8
Chernikov Leonid <leonidcernikov43@gmail.com>
2026-09-24 20:18:31 +0300

    [TASK-1] calc implement

--- leonid.chernikov/task-1/cmd/main.go
+++ leonid.chernikov/task-1/cmd/main.go
@@ -1,6 +1,6 @@
 package main
 
-import(
+import (
 	"bufio"
 	"fmt"
 	"os"
@@ -9,4 +9,53 @@ import(
 
 func main() {
 
+	scanner := bufio.NewScanner(os.Stdin)
+	if !scanner.Scan() {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	first_operand_line := scanner.Text()
+	first_operand_num, err := strconv.Atoi(first_operand_line)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	if !scanner.Scan() {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	second_operand_line := scanner.Text()
+	second_operand_num, err := strconv.Atoi(second_operand_line)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	if !scanner.Scan() {
+		fmt.Println("Invalid operation")
+		return
+	}
+	operation := scanner.Text()
+
+	var result int
+	switch operation {
+	case "/":
+		if second_operand_num == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		result = first_operand_num / second_operand_num
+	case "*":
+		result = first_operand_num * second_operand_num
+	case "+":
+		result = first_operand_num + second_operand_num
+	case "-":
+		result = first_operand_num - second_operand_num
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	fmt.Println(result)
 }
